2fe56f8193c454955a2bbb6cc71788f1f6011a73
Chen3018 <chenjia@LAPTOP-A7QR1HLO>
2024-01-24 13:25:19 -0500

    Created home.ts

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,64 +1,65 @@
 'use strict';
-
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 const url = require('url');
-
 const plugins = require('../plugins');
 const meta = require('../meta');
 const user = require('../user');
-
 function adminHomePageRoute() {
     return ((meta.config.homePageRoute === 'custom' ? meta.config.homePageCustom : meta.config.homePageRoute) || 'categories').replace(/^\//, '');
 }
-
-async function getUserHomeRoute(uid) {
-    const settings = await user.getSettings(uid);
-    let route = adminHomePageRoute();
-
-    if (settings.homePageRoute !== 'undefined' && settings.homePageRoute !== 'none') {
-        route = (settings.homePageRoute || route).replace(/^\/+/, '');
-    }
-
-    return route;
+function getUserHomeRoute(uid) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const settings = yield user.getSettings(uid);
+        let route = adminHomePageRoute();
+        if (settings.homePageRoute !== 'undefined' && settings.homePageRoute !== 'none') {
+            route = (settings.homePageRoute || route).replace(/^\/+/, '');
+        }
+        return route;
+    });
 }
-
-async function rewrite(req, res, next) {
-    if (req.path !== '/' && req.path !== '/api/' && req.path !== '/api') {
-        return next();
-    }
-    let route = adminHomePageRoute();
-    if (meta.config.allowUserHomePage) {
-        route = await getUserHomeRoute(req.uid, next);
-    }
-
-    let parsedUrl;
-    try {
-        parsedUrl = url.parse(route, true);
-    } catch (err) {
-        return next(err);
-    }
-
-    const { pathname } = parsedUrl;
-    const hook = `action:homepage.get:${pathname}`;
-    if (!plugins.hooks.hasListeners(hook)) {
-        req.url = req.path + (!req.path.endsWith('/') ? '/' : '') + pathname;
-    } else {
-        res.locals.homePageRoute = pathname;
-    }
-    req.query = Object.assign(parsedUrl.query, req.query);
-
-    next();
+function rewrite(req, res, next) {
+    return __awaiter(this, void 0, void 0, function* () {
+        if (req.path !== '/' && req.path !== '/api/' && req.path !== '/api') {
+            return next();
+        }
+        let route = adminHomePageRoute();
+        if (meta.config.allowUserHomePage) {
+            route = yield getUserHomeRoute(req.uid);
+        }
+        let parsedUrl;
+        try {
+            parsedUrl = url.parse(route, true);
+        }
+        catch (err) {
+            return next(err);
+        }
+        const { pathname } = parsedUrl;
+        const hook = `action:homepage.get:${pathname}`;
+        if (!plugins.hooks.hasListeners(hook)) {
+            req.url = req.path + (!req.path.endsWith('/') ? '/' : '') + pathname;
+        }
+        else {
+            res.locals.homePageRoute = pathname;
+        }
+        req.query = Object.assign(parsedUrl.query, req.query);
+        next();
+    });
 }
-
 exports.rewrite = rewrite;
-
 function pluginHook(req, res, next) {
     const hook = `action:homepage.get:${res.locals.homePageRoute}`;
-
     plugins.hooks.fire(hook, {
         req: req,
         res: res,
         next: next,
     });
 }
-
 exports.pluginHook = pluginHook;
